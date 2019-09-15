c3e7af0114e8e6b7c5385ef10ae2a48be0a68d37
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2019-09-14 23:25:02 -0300

    Add files via upload

---
+++ recog_face.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+#foto = cv2.imread("pessoas.jpg")
+cap = cv2.VideoCapture(0)
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+while True:
+
+        _, foto = cap.read()
+        gray = cv2.cvtColor(foto, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+        for (x,y,w,h) in faces:
+                cv2.rectangle(foto, (x,y),(x+w,y+h), (0,0,255), 2)
+                
+        cv2.imshow("Video", foto)
+        key = cv2.waitKey(1)
+
+        if key == 27:
+                break
+
+
+cap.release()
+cv2.destroyAllWindows()
